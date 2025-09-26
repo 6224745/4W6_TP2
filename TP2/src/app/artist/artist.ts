3ed5f5b5b004d22ef7966734acd1a3370b12c28d
Gerard Ballestero Vidal <6224745@cegepmontpetit.ca>
2025-09-26 09:11:36 -0400

    FCT requête pour les artists

--- TP2/src/app/artist/artist.ts
+++ TP2/src/app/artist/artist.ts
@@ -1,11 +1,53 @@
 import { Component } from '@angular/core';
+import { Artist } from '../models/artist';
+import { SpotifyService } from '../services/spotify';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-artist',
-  imports: [],
+  imports: [CommonModule, FormsModule, RouterModule],
   templateUrl: './artist.html',
   styleUrl: '../../styles.css'
 })
 export class ArtistComponent {
 
+  artistName: string = "";
+  artiste?: Artist;
+  tabArtist: Artist[] = [];
+
+  constructor(public spotifyservice: SpotifyService) {}
+
+  ngOnInit() {
+    this.spotifyservice.connect();
+    this.loadFavoriteArtists();
+  }
+
+  async getArtist(): Promise<void> {
+    this.artiste = await this.spotifyservice.getArtist(this.artistName);
+    if(this.artiste)
+    {
+      const existArtist = this.tabArtist.find(a => a.id == this.artiste?.id);
+      if(!existArtist)
+      {
+        this.tabArtist.push(this.artiste);
+      }
+    }
+    this.artistName = ""
+    localStorage.setItem("TabArtists", JSON.stringify(this.tabArtist));
+    console.log(this.tabArtist)
+  }
+
+  viderTab() {
+    this.tabArtist = [];
+    localStorage.removeItem("TabArtists")
+  }
+
+  loadFavoriteArtists() {
+    const favorites = localStorage.getItem('TabArtists');
+    if (favorites) {
+      this.tabArtist = JSON.parse(favorites);
+    }
+  }
 }
--- TP2/src/app/services/spotify.ts
+++ TP2/src/app/services/spotify.ts
@@ -1,8 +1,47 @@
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { lastValueFrom } from 'rxjs';
+import { Artist } from '../models/artist';
+
+
+const CLIENT_ID: string = "330ef220cf864f509e3ccb791e610f3d";
+const CLIENT_SECRET: string = "69bc53ef905e47a485aec939fa920b13";
 
 @Injectable({
   providedIn: 'root'
 })
-export class Spotify {
-  
+export class SpotifyService {
+
+  spotifyToken: string | null = null;
+
+  constructor(public http: HttpClient) {}
+
+  async connect() : Promise<void> {
+    
+    let body = new HttpParams().set('grant_type', 'client_credentials');
+    let httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Authorization': 'Basic ' + btoa(CLIENT_ID + ':' + CLIENT_SECRET)
+      })
+    };
+    let x = await lastValueFrom(this.http.post<any>('https://accounts.spotify.com/api/token', body.toString(), httpOptions))
+    console.log(x);
+    this.spotifyToken = x.access_token;
+  }
+
+  async getArtist(artistName : string): Promise<Artist> {
+
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + this.spotifyToken
+      })
+    };
+
+    let x = await lastValueFrom(this.http.get<any>('https://api.spotify.com/v1/search?type=artist&offset=0&limit=1&q=' + artistName, httpOptions));
+    console.log(x);
+
+    return new Artist(x.artists.items[0].id, x.artists.items[0].name, x.artists.items[0].images[0].url);
+  }
 }
